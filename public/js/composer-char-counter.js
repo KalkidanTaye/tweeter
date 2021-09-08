94bd3cf94f6a6a851bef552ebec180d21dfd0711
KalkidanTaye <kali.taye@gmail.com>
2021-09-08 09:59:44 +0000

    implement character count

---
+++ public/js/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+  const textArea = document.getElementById("tweet-textarea");
+  textArea.addEventListener("input", function () {
+    $("#counter").text(140 - $(this).val().length);
+    if ($(this).val().length > 140) {
+      $("#counter").css("color", "red");
+    }
+  });
+});
