1067634008271755c768199d19efdee7071f06d8
KalkidanTaye <kali.taye@gmail.com>
2021-09-09 17:30:09 +0000

    refactor client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,33 +3,76 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+const tweetData = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
+
+//------------- Create Tweet Function ----------------------
+
 const createTweetElement = function (data) {
-  console.log(data);
-  const date = data.created_at;
-  const item = ` <img class= "avatar" src = "${data.user.avatars}">
-                    <label class="tweeter-name">${data.user.name}</label>
-                    <label class="tweeter-username">${data.user.handle}</label>`;
-  console.log(timeago.format(date));
-
-  const item1 = `${data.content.text}`;
-  const day = `${timeago.format(data.created_at)}`;
-  return [item, item1, day];
+  const item = ` <div id="inner-tweets-container">
+                    <div id="tweeter-profile">
+                        <img class= "avatar" src = "${data.user.avatars}">
+                        <label class="tweeter-name">${data.user.name}</label>
+                        <label class="tweeter-username">${
+                          data.user.handle
+                        }</label>
+                    </div>
+                    <div class="display-tweet">
+                        <article class="tweet-text">${
+                          data.content.text
+                        }</article>
+                        <hr />
+                    </div>
+                    <div id="tweet-footer">
+                        <label id="tweet-day">${timeago.format(
+                          data.created_at
+                        )}</label>
+                        <div class="tweet-icons">
+                            <i class="fas fa-flag"></i>
+                            <i class="fas fa-retweet"></i>
+                            <i class="fas fa-heart"></i>
+                        </div>
+                    </div>
+        
+                </div>`;
+  return [item];
 };
 
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
-  },
-  created_at: 1461116232227,
+//------------- Render Tweet Function ----------------------
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  let tweet = [];
+  for (const key in tweets) {
+    tweet = createTweetElement(tweets[key]);
+
+    $("#tweets-container").append(tweet[0]);
+  }
 };
 
-const $tweet = createTweetElement(tweetData);
-
-$("#tweeter-profile").append($tweet[0]); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-$(".tweet-text").append($tweet[1]); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-$("#tweet-day").append($tweet[2]);
+renderTweets(tweetData);
