6c764f778458d0fef0d13dda5c51ff8c752d022a
Kalkidan Alemu <kali.taye@gmail.com>
2021-09-15 13:26:09 -0400

    move helper functions from DOM ready function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,85 +1,90 @@
-$(document).ready(() => {
-  //------------- Create Tweet Function ----------------------
+$(document).ready(() => { 
+  loadTweets();
+  
+});
 
-  const createTweetElement = function (data) {
-    const item = ` <div id="inner-tweets-container">
-                    <div id="tweeter-profile">
-                        <img class= "avatar" src = "${data.user.avatars}">
-                        <label class="tweeter-name">${data.user.name}</label>
-                        <label class="tweeter-username">${
-                          data.user.handle
-                        }</label>
-                    </div>
-                    <div class="display-tweet">
-                        <article class="tweet-text">${escape(
-                          data.content.text
-                        )}</article>
-                        <hr />
-                    </div>
-                    <div id="tweet-footer">
-                        <label id="tweet-day">${timeago.format(
-                          data.created_at
-                        )}</label>
-                        <div class="tweet-icons">
-                            <i class="fas fa-flag"></i>
-                            <i class="fas fa-retweet"></i>
-                            <i class="fas fa-heart"></i>
-                        </div>
-                    </div>
-        
-                </div>`;
-    return item;
-  };
+ //------------- Create Tweet Function ----------------------
+const createTweetElement = function (data) {
+  const item = ` <div id="inner-tweets-container">
+                  <div id="tweeter-profile">
+                      <img class= "avatar" src = "${data.user.avatars}">
+                      <label class="tweeter-name">${data.user.name}</label>
+                      <label class="tweeter-username">${
+                        data.user.handle
+                      }</label>
+                  </div>
+                  <div class="display-tweet">
+                      <article class="tweet-text">${escape(
+                        data.content.text
+                      )}</article>
+                      <hr />
+                  </div>
+                  <div id="tweet-footer">
+                      <label id="tweet-day">${timeago.format(
+                        data.created_at
+                      )}</label>
+                      <div class="tweet-icons">
+                          <i class="fas fa-flag"></i>
+                          <i class="fas fa-retweet"></i>
+                          <i class="fas fa-heart"></i>
+                      </div>
+                  </div>
+      
+              </div>`;
+  return item;
+};
 
-  //------------- Render Tweet Function ----------------------
+ //------------- Render Tweet Function ----------------------
 
-  const renderTweets = function (tweets) {
-    let tweet;
-    $("#tweets-container").empty();
-    for (const key in tweets) {
-      tweet = createTweetElement(tweets[key]);
-      $("#tweets-container").prepend(tweet);
-    }
-    
-  };
-  //----------------- fetch tweet --------------------
+ const renderTweets = function (tweets) {
+  let tweet;
+  $("#tweets-container").empty();
+  for (const key in tweets) {
+    tweet = createTweetElement(tweets[key]);
+    $("#tweets-container").prepend(tweet);
+  }
+  
+};
 
-  const loadTweets = function () {
+ //----------------- fetch tweet --------------------
+
+ const loadTweets = function () {
+  $.ajax({
+    url: "/tweets",
+    method: "GET",
+    dataType: "json",
+  })
+    .then(function (tweets) {
+      renderTweets(tweets);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+};
+
+//----------------- submit tweet --------------------
+
+$("form").on("submit", (event) => {
+  event.preventDefault();
+  if (formValidation()) {
+    let str = $("form").serialize();
     $.ajax({
       url: "/tweets",
-      method: "GET",
-      dataType: "json",
+      method: "POST",
+      data: str,
     })
-      .then(function (tweets) {
-        renderTweets(tweets);
+      .then(function (response) {
+        loadTweets();
       })
       .catch(function (err) {
         console.log(err);
       });
-  };
-  loadTweets();
-  //----------------- submit tweet --------------------
+    $("#tweet-textarea").val("").focus();
+    $("#counter").val("140");
+  }
+  
+});
 
-  $("form").on("submit", (event) => {
-    event.preventDefault();
-    if (formValidation()) {
-      let str = $("form").serialize();
-      $.ajax({
-        url: "/tweets",
-        method: "POST",
-        data: str,
-      })
-        .then(function (response) {
-          loadTweets();
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-      $("#tweet-textarea").val("").focus();
-      $("#counter").val("140");
-    }
-    
-  });
 
   //---------- form validation ---------------
 
@@ -105,15 +110,14 @@ $(document).ready(() => {
     return div.innerHTML;
   };
 
-  //------ form slide up or down -------- not working!
-  const $arrow = document.getElementsByClassName(".fa-angle-double-down");
-  $(".fa-angle-double-down").click = function () {
-    const $toggleText = document.getElementsByClassName(".new-tweet");
-    if ($toggleText.style.display !== "none") {
-      console.log("None");
-      $toggleText.style.display = "none";
-    }
-    $toggleText.style.display = "flex";
-    console.log("Display Yes");
-  };
-});
+//------ form slide up or down -------- not working!
+const $arrow = document.getElementsByClassName(".fa-angle-double-down");
+$(".fa-angle-double-down").click = function () {
+  const $toggleText = document.getElementsByClassName(".new-tweet");
+  if ($toggleText.style.display !== "none") {
+    console.log("None");
+    $toggleText.style.display = "none";
+  }
+  $toggleText.style.display = "flex";
+  console.log("Display Yes");
+};
